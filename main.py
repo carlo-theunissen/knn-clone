fc8a5d95e257e7af3c90f32220b1fb0e682bbf75
Yaroslav Nazarov <yaroslavn99@gmail.com>
2021-02-15 11:12:51 +0100

    Ties are now broken by the order in which point appear in the dataset

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import math
 import pandas as pd
 import matplotlib.pyplot as plt
 from joblib import Parallel, delayed
+import numpy as np
+import time
 
 
 def cartesian(x_1, x_2):
@@ -26,7 +28,7 @@ def knn(train_X, train_y, test_X, metric, k, current_idx=None):
         # Get the labels of the nearest neighbours
         nearest_labels = [train_y[distance[0]] for distance in distances]
         # Break ties in ascending order
-        most_commons = sorted(Counter(nearest_labels).most_common(), key=lambda x: (-x[1], x[0]))
+        most_commons = Counter(nearest_labels).most_common()
         most_common = most_commons[0][0]
 
         output.append(most_common)
@@ -35,12 +37,15 @@ def knn(train_X, train_y, test_X, metric, k, current_idx=None):
 
 
 if __name__ == '__main__':
-    train_data_X = pd.read_csv("MNIST_train_small.csv").to_numpy()[:, 1:]
-    train_data_y = pd.read_csv("MNIST_train_small.csv").to_numpy()[:, 0]
-    test_data_X = pd.read_csv("MNIST_test_small.csv").to_numpy()[:, 1:]
-    test_data_y = pd.read_csv("MNIST_test_small.csv").to_numpy()[:, 0]
+    train_data_X = np.repeat(pd.read_csv("MNIST_train_small.csv").to_numpy()[:, 1:], repeats=20, axis=0)
+    train_data_y = np.repeat(pd.read_csv("MNIST_train_small.csv").to_numpy()[:, 0], repeats=20, axis=0)
+    test_data_X = np.repeat(pd.read_csv("MNIST_test_small.csv").to_numpy()[:, 1:], repeats=1, axis=0)
+    test_data_y = np.repeat(pd.read_csv("MNIST_test_small.csv").to_numpy()[:, 0], repeats=1, axis=0)
 
     n_cores = multiprocessing.cpu_count()
 
-    results = Parallel(n_jobs=n_cores)(delayed(knn)(train_data_X, train_data_y, [test_point], cartesian, 3, idx)
+    t0 = time.time()
+    Parallel(n_jobs=n_cores)(delayed(knn)(train_data_X, train_data_y, [test_point], cartesian, 3, idx)
                                        for idx, test_point in enumerate(test_data_X))
+
+    print(time.time()-t0)
